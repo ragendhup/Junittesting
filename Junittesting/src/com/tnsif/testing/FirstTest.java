4842047a630dfabb85692009dc809820838ce787
ragendhu <ragendhu@DESKTOP-0F8M4MR>
2024-09-02 20:53:04 +0530

    first commit

---
+++ Junittesting/src/com/tnsif/testing/FirstTest.java
@@ -0,0 +1,14 @@
+package com.tnsif.testing;
+
+import org.junit.jupiter.api.Test;
+//@Test not allowed
+public class FirstTest {
+@Test
+	void display() {
+		System.out.println("my test method");
+	}
+@Test
+void Hello() {
+	System.out.println("greet");
+}
+}
